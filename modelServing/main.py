2e1ddac939fb1a65f8626cd0d4de510e1e8751f6
solsoleee <wsw3727@gmail.com>
2024-03-27 22:37:09 +0900

    model 배포 준비

--- modelServing/main.py
+++ modelServing/main.py
@@ -1,6 +1,37 @@
+from fastapi import FastAPI
 import uvicorn
+import pickle
+import pandas as pd
+import numpy as np
+from pydantic import BaseModel
 
-if __name__ == "__main__":
+with open("model_and_vectorizer.dump", "rb")as f:
+    loaded_data = pickle.load(f)
+
+
+# 불러온 데이터에서 모델과 벡터라이저를 각각 추출
+loaded_model = loaded_data['model']
+loaded_vectorizer = loaded_data['vectorizer']
+
+app = FastAPI(title="ML API")
+
+class Politics(BaseModel):
+    title: str
+    content: str
+
+# 새 데이터의 '제목'과 '내용'을 결합
+
+
+
+@app.post("/predict", status_code=200)
+async def predict_tf(x: Politics):
+    new_data = pd.DataFrame({"제목": [x.title], "내용": [x.content]})
+    X_new = new_data['제목'] + ' ' + new_data['내용']
+    X_new_tfidf = loaded_vectorizer.transform(X_new)
+    res= loaded_model.predict(X_new_tfidf)
     
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, log_level="debug",
-               proxy_headers=True, reload=True)
+    return {"prediction": res}
+
+@app.get('/')
+async def root():
+    return {"message": "online"}
