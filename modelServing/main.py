a1c3a31b1cc091285662588bb18c483757c470d7
solsoleee <wsw3727@gmail.com>
2024-03-27 22:30:06 +0900

    model 배포 준비

--- modelServing/main.py
+++ modelServing/main.py
@@ -2,5 +2,5 @@ import uvicorn
 
 if __name__ == "__main__":
     
-    uvicorn.run("app.main:app", host="0.0.0.0", port=9999, log_level="debug",
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, log_level="debug",
                proxy_headers=True, reload=True)
