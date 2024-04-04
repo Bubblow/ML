46794ea66277fb2c57cecdc7385b9fc113dfb9fd
solsoleee <wsw3727@gmail.com>
2024-04-04 14:58:59 +0900

    모델 확률값 반환

--- modelServing/main.py
+++ modelServing/main.py
@@ -38,8 +38,7 @@ async def predict_tf(x: Politics):
     X_new_tfidf = loaded_vectorizer.transform(new_data['data'])
     res = loaded_model.predict(X_new_tfidf)
     percentage = loaded_model.predict_proba(X_new_tfidf)
-    percentage0 = str(percentage[0])
-    percentage1 = str(percentage[1])
+    percentage0, percentage1 = next(iter(percentage.items()))
     
     return {"prediction": res.tolist(), "percentage0": percentage0, "percentage1": percentage1}
 
